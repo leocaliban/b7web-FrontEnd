681ab849daf3b8e38d4b1983ece7bd13fb4991ad
Leocaliban <leocaliban@gmail.com>
2019-01-18 20:15:51 -0300

    Declaração de classe

---
+++ 06 - ADVANCED JAVASCRIPT/02_declaracao_de_classe/script.js
@@ -0,0 +1,14 @@
+// Objeto pessoas com atributos pré-definidos
+function Pessoa() {
+    this.nome = "Aline";
+    this.peso = "50";
+    this.idade = "20";
+}
+
+// Instanciação de uma pessoa
+var pessoa = new Pessoa();
+
+// Modificação de atributos da pessoa
+pessoa.nome = "Amanda";
+
+
