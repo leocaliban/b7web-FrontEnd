0abf2721a3ba2a8de648bade3c021aad29692902
Leocaliban <leocaliban@gmail.com>
2019-01-20 06:58:01 -0300

    [FAQ] Adicionar ou remover atributos de elementos

---
+++ 07 - JQUERY/11_FAQ/03_ativar_ou_desativar_elementos/assets/javascript/script.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    // Adicionar atributo
+    
+    $('#box').attr('disabled', 'disabled');
+
+    // Remover atributo
+    $('#box').removeAttr('disabled', 'disabled');
+
+    $('#box').attr('checked', 'checked');
+
+});
