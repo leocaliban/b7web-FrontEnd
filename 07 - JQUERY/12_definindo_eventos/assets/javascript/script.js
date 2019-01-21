20468ed917aeeb5ba09dc304bdcc0cb8ebccb226
Leocaliban <leocaliban@gmail.com>
2019-01-21 02:29:08 -0300

    Definindo Eventos em Elementos DOM

---
+++ 07 - JQUERY/12_definindo_eventos/assets/javascript/script.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    $('button').click(function () {
+        if ($('h1').hasClass('fundo')) {
+            $('h1').removeClass('fundo');
+        } else {
+            $('h1').addClass('fundo');
+        }
+
+        // Para ações de ativar ou desativar usar apenas
+        // $('h1').toggleClass('fundo'); para realizar a alternância
+
+    });
+    
+    $('button').mouseover(function(){
+        $(this).addClass('fundo');
+    });
+
+    $('button').mouseout(function(){
+        $(this).removeClass('fundo');
+    });
+});
