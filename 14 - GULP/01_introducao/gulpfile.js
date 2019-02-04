d2949f732fd1ef5d735c25c086e7b70e792bf057
Leocaliban <leocaliban@gmail.com>
2019-02-04 19:57:49 -0300

    Minificando CSS

--- 14 - GULP/01_introducao/gulpfile.js
+++ 14 - GULP/01_introducao/gulpfile.js
@@ -1,14 +1,30 @@
 
-const { src, dest } = require('gulp');
+const { src, dest, parallel } = require('gulp');
 
 const rename = require('gulp-rename');
 const uglifyJS = require('gulp-uglify');
+const uglifyCSS = require('gulp-uglifycss');
 
-exports.default = () => {
+
+function javascript() {
     return src('src/js/*.js')
         .pipe(uglifyJS())
         .pipe(rename({
             extname: '.min.js'
         }))
-        .pipe(dest('dist/'));
-};
+        .pipe(dest('dist/assets/js'));
+}
+function css() {
+    return src('src/css/*.css')
+        .pipe(uglifyCSS({
+            'maxLineLen': 80,
+            'uglyComments': true
+
+        }))
+        .pipe(rename({
+            extname: '.min.css'
+        }))
+        .pipe(dest('dist/assets/css'));
+}
+
+exports.default = parallel(javascript, css);
