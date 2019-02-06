68c5ee04a07f706b95c0f95b0452440e4d764ab7
Leocaliban <leocaliban@gmail.com>
2019-02-06 04:07:41 -0300

    Aplica browser-sync

--- 14 - GULP/02_workflow_basico/gulpfile.js
+++ 14 - GULP/02_workflow_basico/gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 const htmlMIN = require('gulp-htmlmin');
 const scssMIN = require('gulp-sass');
 const notify = require('gulp-notify');
+let browserSync = require('browser-sync').create();
+
 
 // sass
 gulp.task('sass', () => {
@@ -10,24 +12,34 @@ gulp.task('sass', () => {
             outputStyle: 'compressed'
         }))
         .on('error', notify.onError('Erro: <%= error.message %>'))
-        .pipe(gulp.dest('dist/assets/css'));
+        .pipe(gulp.dest('dist/assets/css'))
+        .pipe(browserSync.stream());
 });
 
-
+// html
 gulp.task('html', () => {
     return gulp.src('src/index.html')
         .pipe(htmlMIN({
             collapseInlineTagWhitespace: true
         }))
         .on('error', notify.onError('Erro: <%= error.message %>'))
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.stream());
 });
 
+// watch
 gulp.task('watch', () => {
     gulp.watch('src/index.html', gulp.task('html'));
     gulp.watch('src/assets/scss/style.scss', gulp.task('sass'));
 });
 
-gulp.task('build', gulp.series(gulp.parallel('html', 'sass'), 'watch'), () => {
-
+// sync
+gulp.task('SYNC', () => {
+    browserSync.init({
+        server: {
+            baseDir: 'dist/'
+        }
+    });
 });
+
+gulp.task('build', gulp.series(gulp.parallel('html', 'sass'), gulp.parallel('SYNC', 'watch')));
